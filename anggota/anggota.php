5c003c37a196d3ad669fd6de0cec75fbc2e03dfd
alfinilmi <80630241+alfinilmi@users.noreply.github.com>
2022-05-30 11:28:09 +0700

    initial

---
+++ anggota/anggota.php
@@ -0,0 +1,53 @@
+<?php include '../design/awal.php'; ?>
+						<div class="col-lg-4 p-l-0 title-margin-left">
+	                        <div class="page-header">
+	                            <div class="page-title">
+	                                <ol class="breadcrumb">
+	                                    <li class="breadcrumb-item"><a href="#">Halaman Anggota</a></li>
+	                                </ol>
+	                            </div>
+	                        </div>
+	                    </div>
+	                    <!-- /# column -->
+	                </div>
+	                <!-- /row -->
+
+	                <section id="main-content">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="card-title">
+                                    <h3 class="color-primary">Data Anggota</h3>
+                                </div>
+	                            <div class="card">
+	                                <div class="card-body">
+	                                    <div class="table-responsive">
+	                                    	<a href="#"><button type="button" class="btn btn-primary btn-flat btn-addon m-b-10 m-l-5"><i class="ti-plus"></i>Tambah Data</button></a>
+	                                        <table class="table table-bordered">
+	                                            <thead>
+	                                                <tr>
+	                                                    <th>No.</th>
+	                                                    <th>Nama</th>
+	                                                    <th>Jumlah</th>
+	                                                </tr>
+	                                            </thead>
+	                                            <tbody>
+	                                                <tr>
+	                                                    <th scope="row">1</th>
+	                                                    <td>Kalau mau ada warna</td>
+	                                                    <td class="color-primary">$21.56</td>
+	                                                </tr>
+	                                                <tr>
+	                                                    <th scope="row">2</th>
+	                                                    <td>Kalo gamau ada warna</td>
+	                                                    <td>$55.32</td>
+	                                                </tr>
+	                                            </tbody>
+	                                        </table>
+	                                    </div>
+	                                </div>
+	                            </div>
+	                        </div>
+	                        <!-- /# column -->
+                        </div>
+                        <!-- /# row -->
+<?php include '../design/akhir.php'; ?>
---
+++ design/awal.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Aplikasi Bendahara</title>
+
+        <!-- ================= Favicon ================== -->
+        <!-- Standard -->
+        <link rel="shortcut icon" href="http://placehold.it/64.png/000/fff">
+        <!-- Retina iPad Touch Icon-->
+        <link rel="apple-touch-icon" sizes="144x144" href="http://placehold.it/144.png/000/fff">
+        <!-- Retina iPhone Touch Icon-->
+        <link rel="apple-touch-icon" sizes="114x114" href="http://placehold.it/114.png/000/fff">
+        <!-- Standard iPad Touch Icon-->
+        <link rel="apple-touch-icon" sizes="72x72" href="http://placehold.it/72.png/000/fff">
+        <!-- Standard iPhone Touch Icon-->
+        <link rel="apple-touch-icon" sizes="57x57" href="http://placehold.it/57.png/000/fff">
+
+        <!-- Styles -->
+        <link href="/uas_web/assets/css/lib/weather-icons.css" rel="stylesheet" />
+        <link href="/uas_web/assets/css/lib/owl.carousel.min.css" rel="stylesheet" />
+        <link href="/uas_web/assets/css/lib/owl.theme.default.min.css" rel="stylesheet" />
+        <link href="/uas_web/assets/css/lib/font-awesome.min.css" rel="stylesheet">
+        <link href="/uas_web/assets/css/lib/themify-icons.css" rel="stylesheet">
+        <link href="/uas_web/assets/css/lib/menubar/sidebar.css" rel="stylesheet">
+        <link href="/uas_web/assets/css/lib/bootstrap.min.css" rel="stylesheet">
+
+        <link href="/uas_web/assets/css/lib/helper.css" rel="stylesheet">
+        <link href="/uas_web/assets/css/style.css" rel="stylesheet">
+    </head>
+
+    <body>
+
+        <div class="sidebar sidebar-hide-to-small sidebar-shrink sidebar-gestures">
+            <div class="nano">
+                <div class="nano-content">
+                    <div class="logo"><a href="/uas_web"><!-- <img src="assets/images/logo.png" alt="" /> --><span>Focus</span></a></div>
+                    <ul>
+                        <li class="label">Main</li>
+                        <li><a href="/uas_web"><i class="ti-home"></i> Home </a></li>
+
+                        <li class="label">Apps</li>
+                        <li><a href="/uas_web/anggota/anggota.php"><i class="ti-user"></i> Anggota </a></li>
+                        <li><a href="/uas_web/kas/kas.php"><i class="ti-money"></i> Kas </a></li>
+                        <li><a href="/uas_web/pengeluaran/pengeluaran.php"><i class="ti-clipboard"></i> Pengeluaran </a></li>
+                        <li><a href="/uas_web/laporan/laporan.php"><i class="ti-book"></i> Laporan </a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- /# sidebar -->
+
+
+        <div class="header">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="float-left">
+                            <div class="hamburger sidebar-toggle">
+                                <span class="line"></span>
+                                <span class="line"></span>
+                                <span class="line"></span>
+                            </div>
+                        </div>
+                        <div class="float-right">
+                            <ul>
+                                <li class="header-icon dib"><span class="user-avatar">Bendahara <i class="ti-angle-down f-s-10"></i></span>
+                                    <div class="drop-down dropdown-profile">
+                                        <div class="dropdown-content-body">
+                                            <ul>
+                                                <li><a href="#"><i class="ti-power-off"></i> <span>Logout</span></a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="content-wrap">
+            <div class="main">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-8 p-r-0 title-margin-right">
+                            <div class="page-header">
+                                <div class="page-title">
+                                    <h1>Hai, <span>Cantik</span></h1>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /# column -->
